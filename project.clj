80a4ee08390d146425c61c11c212aa69434d7459
sukuizhang <skz@skzdeMacBook-Pro.local>
2012-03-29 19:01:38 +0800

    can work only

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject lein-easyconf "1.0.0"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [easyconf "0.1.1-SNAPSHOT"]
+                 [org.clojure/tools.namespace "0.1.0"]]
+  :dev-dependencies [[org.clojure/tools.namespace "0.1.0"]])
---
+++ src/leiningen/gen_conf.clj
@@ -0,0 +1,13 @@
+(ns leiningen.gen-conf
+  (:require [leiningen.compile :as eval]))
+
+(defn gen-conf
+  [project & [path]]
+  (prn project)
+  (let [path (or path
+                 (:test-path project)
+                 "etc")])
+  (eval/eval-in-subprocess
+   project
+   (str "(do (require 'easyconf.gen)
+             (easyconf.gen/gen-conf \"" path "\"))")))
